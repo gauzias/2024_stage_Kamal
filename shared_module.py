de30a6a1f36acbdbe01d8ae4ba341101fb8331d5
Kamal Achalhi <Kamal842010@hotmail.fr>
2024-07-29 12:10:23 +0200

    Revert "Mise à jour contribution"

--- shared_module.py
+++
@@ -1,9 +0,0 @@
-import ants
-
-tx = ants.new_ants_transform(dimension=2)
-
-tx.set_parameters((0.9,0,0,1.1,10,11))
-
-ants.write_transform(tx, '/envau/work/meca/users/2024_Kamal/output/output_script1/tx.mat')
-
-tx2 = ants.read_transform('/envau/work/meca/users/2024_Kamal/output/output_script1/tx.mat')
